0c6020cc9b9fae24a65553b2bcde3143495b55d5
gc729757859 <gaocheng741@aliyun.com>
2020-06-08 16:05:25 +0800

    Initial commit

---
+++ yoga1/src/main/java/com/yoga/Yoga1Application.java
@@ -0,0 +1,13 @@
+package com.yoga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yoga1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yoga1Application.class, args);
+    }
+
+}
