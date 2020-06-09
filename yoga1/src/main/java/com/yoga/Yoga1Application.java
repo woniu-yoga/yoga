62d5d255c79294db0715d6e07d7ecbd704c41d93
253846410 <253846410@qq.com>
2020-06-09 15:02:53 +0800

    userinfo service

--- yoga1/src/main/java/com/yoga/Yoga1Application.java
+++ yoga1/src/main/java/com/yoga/Yoga1Application.java
@@ -1,9 +1,11 @@
 package com.yoga;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yoga.mapper")
 public class Yoga1Application {
 
     public static void main(String[] args) {
