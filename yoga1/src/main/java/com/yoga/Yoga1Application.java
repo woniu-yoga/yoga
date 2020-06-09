6230d4e5bd1c1e7b5bad363786241089ea5bf7b4
253846410 <253846410@qq.com>
2020-06-09 20:45:55 +0800

    mybatis逆向:bai

--- yoga1/src/main/java/com/yoga/Yoga1Application.java
+++ yoga1/src/main/java/com/yoga/Yoga1Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.yoga.mapper")
 public class Yoga1Application {
 
     public static void main(String[] args) {
