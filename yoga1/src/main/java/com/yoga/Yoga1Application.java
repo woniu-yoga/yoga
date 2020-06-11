0dabe151f8a6b1a60f1fe4372578d136a7d7024d
253846410 <253846410@qq.com>
2020-06-11 14:47:06 +0800

    我的钱包模块逆向修改

--- yoga1/src/main/java/com/yoga/Yoga1Application.java
+++ yoga1/src/main/java/com/yoga/Yoga1Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.yoga.mapper")
 public class Yoga1Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Yoga1Application.class, args);
     }
